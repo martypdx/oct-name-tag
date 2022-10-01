fbb7ce8babc2d7b49d16096b59741552db4ab416
Julie Nisbet <jmnisbet@gmail.com>
2022-10-01 15:28:20 -0700

    adds input interactivity

--- app.js
+++ app.js
@@ -1,39 +1,21 @@
-console.log('hello world');
+// DOM - document object model
+// built into your browser -- its a JS representation of your HTML
 
-// let dogName = 'Finley';
-// let age = 3;
-// let breed = 'Golden Doodle';
+// DOM GETTER METHODS
+// getElementById
+const nameDisplay = document.getElementById('name');
+console.log(nameDisplay);
 
-// console.log(`My dog's name is ${dogName}`);
-// console.log(`${dogName} is very cute.`);
-// console.log(`${dogName} is a ${breed} and is ${age} years old.`);
+// DOM SETTER METHODS
+// textContent
+nameDisplay.textContent = 'Julie';
+nameDisplay.classList.add('purple');
 
-// let dogName2 = 'Benny';
-// let age2 = 7;
-// let breed2 = 'Terrier Mix';
-
-// console.log(`My dog's name is ${dogName2}`);
-// console.log(`${dogName2} is very cute.`);
-// console.log(`${dogName2} is a ${breed2} and is ${age2} years old.`);
-
-// parameters vs arguments
-// parameters are part of function definition
-function describeDog(dogName, age, breed) {
-    console.log(`My dog's name is ${dogName}`);
-    console.log(`${dogName} is very cute.`);
-    console.log(`${dogName} is a ${breed} and is ${age} years old.`);
-}
-
-// arguments are part of the function call
-describeDog('Benny', 7, 'Terrier-Mix');
-describeDog('Finley', 4, 'Golden-Doodle');
-
-// the following function definitions are equivalant
-
-// function myFunc() {
-//     console.log('this is my function');
-// }
-
-// const myFunc = () => {
-//     console.log('this is my function');
-// };
+const updateBtn = document.getElementById('update-btn');
+updateBtn.addEventListener('click', () => {
+    console.log('i am clicking the button');
+    // grab the input element
+    const nameInput = document.getElementById('name-input');
+    // set the display to the value from the input
+    nameDisplay.textContent = nameInput.value;
+});
